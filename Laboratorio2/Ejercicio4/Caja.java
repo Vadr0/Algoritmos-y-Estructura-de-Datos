f8c833a9def2a4c7758e8af414fa6083c10e6e03
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-03-25 07:58:09 -0500

    Create Caja.java

---
+++ Laboratorio2/Ejercicio4/Caja.java
@@ -0,0 +1,24 @@
+package Ejercicio4;
+
+class Caja<T> {
+    private String color;
+    private T contenido;
+
+    public Caja(String color, T contenido) {
+        this.color = color.trim();
+        this.contenido = contenido;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public T getContenido() {
+        return contenido;
+    }
+
+    @Override
+    public String toString() {
+        return "Caja de color " + color + " --> " + contenido;
+    }
+}
