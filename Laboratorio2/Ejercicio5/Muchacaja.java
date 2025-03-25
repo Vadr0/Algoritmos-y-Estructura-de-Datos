d948f207f90013e95b6557a8c18a78584ff247d2
rodel2A <164429797+rodel2A@users.noreply.github.com>
2025-03-25 15:48:51 -0500

    Create Muchacaja.java

---
+++ Laboratorio2/Ejercicio5/Muchacaja.java
@@ -0,0 +1,59 @@
+package ejercicio_5;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Muchacaja<T> implements Iterable<Caja<T>> {
+    private ArrayList<Caja<T>> lista = new ArrayList<>();
+    private int tope;
+
+    public Muchacaja(int tope) {
+        this.tope = tope;
+    }
+
+    public void addCaja(Caja<T> caja) {
+        if (lista.size() < tope) {
+            lista.add(caja);
+        } else {
+            throw new RuntimeException("Ya no entran cosas en el baúl.");
+        }
+    }
+
+    public String search(T objeto) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getContenido().equals(objeto)) {
+                return "Posición: " + (i + 1) + ", Color: " + lista.get(i).getColor();
+            }
+        }
+        return "Objeto no encontrado";
+    }
+
+    public T delete(T objeto) {
+        Iterator<Caja<T>> iter = lista.iterator();
+        while (iter.hasNext()) {
+            Caja<T> caja = iter.next();
+            if (caja.getContenido().equals(objeto)) {
+                iter.remove();
+                return caja.getContenido();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Baúl:\n");
+        for (int i = 0; i < lista.size(); i++) {
+            sb.append("Posición: ").append(i + 1)
+                    .append(", Color -> ").append(lista.get(i).getColor())
+                    .append(", Contenido --> ").append(lista.get(i).getContenido()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public Iterator<Caja<T>> iterator() {
+        return lista.iterator();
+    }
+}
+
