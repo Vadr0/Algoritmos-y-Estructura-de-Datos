f1df1da0458c7d511d29f381e4ac38d6a2a8fbd3
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-04-06 21:08:44 -0500

    Se corrigió el ciclo 'for' en 'moda01' para que recorra correctamente hasta el último índice con 'i <= end', se ajustó la lógica de retorno en 'moda01' para devolver la moda correctamente al final del método.

---
+++ Laboratorio 3 /Actividades/Actividad04/Moda01.java
@@ -0,0 +1,41 @@
+package Actividad04;
+
+public class Moda01 {
+    public static int moda01(int array[]) {
+        int first = 0;
+        int end = array.length - 1;
+
+        // Si el array tiene solo un elemento
+        if (first == end)
+            return array[first]; // Devuelve el único valor
+
+        int moda = array[first];
+        int maxfrec = frecuencia(array, first, end, moda); // Calcula la frecuencia del primer elemento
+
+        // Recorre el array desde el segundo elemento
+        for (int i = first + 1; i <= end; i++) {
+            int frec = frecuencia(array, i, end, array[i]);
+            if (frec > maxfrec) {
+                maxfrec = frec;
+                moda = array[i]; // Actualiza la moda si se encuentra una frecuencia mayor
+            }
+        }
+        return moda; // Devuelve la moda encontrada
+    }
+
+    private static int frecuencia(int[] array, int first, int end, int ele) {
+        int suma = 0;
+        for (int i = first; i <= end; i++) {
+            if (array[i] == ele) {
+                suma++;
+            }
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 2, 2, 3, 3, 3 };
+        int result = moda01(array);
+        System.out.println("La moda es: " + result); // Imprime la moda
+    }
+}
