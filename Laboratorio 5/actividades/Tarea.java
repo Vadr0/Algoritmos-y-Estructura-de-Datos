a089d85763b54b4188de0d8a542c043f5b0c3bb2
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-04-27 20:57:48 -0500

    Creacion de la clase tarea, parte de la actividad

---
+++ Laboratorio 5/actividades/Tarea.java
@@ -0,0 +1,35 @@
+package actividades;
+
+public class Tarea {
+    private String titulo;
+    private int prioridad;
+    private String descripcion;
+
+    public Tarea(String titulo, int prioridad, String descripcion){
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+        this.descripcion = descripcion;
+    }
+
+    //Getters y Setters
+    public String getTitulo() {
+        return titulo;
+    }
+    public int getPrioridad() {
+        return prioridad;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
