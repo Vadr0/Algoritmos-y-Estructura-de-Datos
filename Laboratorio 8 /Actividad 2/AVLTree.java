3659245bcdbf1bb5ed684954c253837813b1290f
rodel2A <164429797+rodel2A@users.noreply.github.com>
2025-05-20 14:58:04 -0500

    Update AVLTree.java

--- Laboratorio 8 /Actividad 2/AVLTree.java
+++ Laboratorio 8 /Actividad 2/AVLTree.java
@@ -6,14 +6,6 @@ import exceptions.ItemNotFound;
 import exceptions.ExceptionIsEmpty;
 import Actividad_1.LinkedBST;
 
-package Actividad_2;
-
-import Node.Node;
-import exceptions.ItemDuplicated;
-import exceptions.ItemNotFound;
-import exceptions.ExceptionIsEmpty;
-import Actividad_1.LinkedBST;
-
 public class AVLTree<E extends Comparable<E>> extends LinkedBST<E> {
     protected static class NodeAVL<E> extends Node<E> {
         int bf; // Factor de balance: -1, 0, 1
