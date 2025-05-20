64223e24cb1a8ed05701134a0fe79a8b537ec0a1
rodel2A <164429797+rodel2A@users.noreply.github.com>
2025-05-20 14:45:02 -0500

    Update exepciones.java

--- Laboratorio 8 /Actividad 1/exepciones.java
+++ Laboratorio 8 /Actividad 1/exepciones.java
@@ -2,37 +2,22 @@
 package exceptions;
 
 public class ExceptionIsEmpty extends Exception {
-    public ExceptionIsEmpty() {
-        super();
-    }
-
-    public ExceptionIsEmpty(String message) {
-        super(message);
-    }
+    public ExceptionIsEmpty() { super(); }
+    public ExceptionIsEmpty(String message) { super(message); }
 }
 ///////////////////////////////////////////////////////////////////////////
 // ItemDuplicated.java
 package exceptions;
 
 public class ItemDuplicated extends Exception {
-    public ItemDuplicated() {
-        super();
-    }
-
-    public ItemDuplicated(String message) {
-        super(message);
-    }
+    public ItemDuplicated() { super(); }
+    public ItemDuplicated(String message) { super(message); }
 }
 ///////////////////////////////////////////////////////////////////////////
 // ItemNotFound.java
 package exceptions;
 
 public class ItemNotFound extends Exception {
-    public ItemNotFound() {
-        super();
-    }
-
-    public ItemNotFound(String message) {
-        super(message);
-    }
+    public ItemNotFound() { super(); }
+    public ItemNotFound(String message) { super(message); }
 }
