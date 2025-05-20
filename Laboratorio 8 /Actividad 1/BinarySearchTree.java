3d1f0773676ecc367caa7bf55ca0b909d221cb3d
rodel2A <164429797+rodel2A@users.noreply.github.com>
2025-05-20 14:44:04 -0500

    Update BinarySearchTree.java

--- Laboratorio 8 /Actividad 1/BinarySearchTree.java
+++ Laboratorio 8 /Actividad 1/BinarySearchTree.java
@@ -1,9 +1,13 @@
 package bstreeInterface;
 
+import exceptions.ExceptionIsEmpty;
+import exceptions.ItemDuplicated;
+import exceptions.ItemNotFound;
+
 public interface BinarySearchTree<E extends Comparable<E>> {
-    void insert(E data);
+    void insert(E data) throws ItemDuplicated;
     boolean search(E data);
-    void delete(E data);
+    void delete(E data) throws ItemNotFound, ExceptionIsEmpty;
     boolean isEmpty();
     void destroy();
     String toString();
