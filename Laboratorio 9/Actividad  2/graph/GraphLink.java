1ec7e4e11fc074d3cce581c350c186124f32a8ce
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-06-05 18:53:57 -0500

    Se agrego un nuevo metodo insertEdge para aristas ponderadas

--- Laboratorio 9/Actividad  2/graph/GraphLink.java
+++ Laboratorio 9/Actividad  2/graph/GraphLink.java
@@ -41,13 +41,34 @@ public class GraphLink<E> {
         origen.listAdj.insertLast(new Edge<>(destino));
     }
 
-    // Para grafos no dirigidos, la unica diferencia es que se inserta en ambas direcciones
+    public void insertEdge(E verOrigen, E verDestino, int peso) {
+        if (verOrigen == null || verDestino == null) {
+            throw new IllegalArgumentException("Los vertices no pueden ser null");
+        }
+        
+        int posOrigen = listVertex.search(new Vertex<>(verOrigen));
+        int posDestino = listVertex.search(new Vertex<>(verDestino));
+
+        if (posOrigen == -1 || posDestino == -1) {
+            return;
+        }
+
+        Vertex<E> origen = listVertex.get(posOrigen);
+        Vertex<E> destino = listVertex.get(posDestino);
+
+        if (origen.listAdj.search(new Edge<>(destino, peso)) != -1) {
+            return;
+        }
+
+        origen.listAdj.insertLast(new Edge<>(destino, peso));
+    }
+
+    // Para grafos no dirigidos, la unica diferencia es que se inserta en ambas
+    // direcciones
     public void insertEdgeUndirected(E verOrigen, E verDestino) {
         insertEdge(verOrigen, verDestino);
         insertEdge(verDestino, verOrigen);
     }
-
-    
 
     // Crear un nuevo metodo insert edge para cuando se necesite verificar peso
     // tambien
