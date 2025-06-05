36bada2f2fa53b33d7a6a23acf4f930338215bd5
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-06-05 14:06:50 -0500

    Se creo el metodo de insercion

--- Laboratorio 9/Actividad  2/graph/GraphLink.java
+++ Laboratorio 9/Actividad  2/graph/GraphLink.java
@@ -1,22 +1,28 @@
 package graph;
+
 import linkedlist.ListLinked;
+
 public class GraphLink<E> {
 
     protected ListLinked<Vertex<E>> listVertex;
 
-    public GraphLink(){
+    public GraphLink() {
         listVertex = new ListLinked<Vertex<E>>();
     }
 
-    public void insertVertex(E data){
+    public void insertVertex(E data) {
+        // Usar search para evitar duplicados
+        if (listVertex.search(new Vertex<>(data)) != -1) {
+            return; // Ya existe el vertice
+        }
+        listVertex.insertLast(new Vertex<>(data));
+    }
+
+    public void insertEdge(E verOrigen, E verDestino) {
 
     }
 
-    public void insertEdge (E verOrigen, E verDestino){
-
-    }
-
-    public String toString(){
+    public String toString() {
         return this.listVertex.toString();
     }
 }
