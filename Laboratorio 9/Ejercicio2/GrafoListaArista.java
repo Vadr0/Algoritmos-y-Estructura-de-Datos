8f23c9daca481ba475272ed798f0fc7e8c9617ab
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-06-10 13:26:13 -0500

    Update GrafoListaArista.java

--- Laboratorio 9/Ejercicio2/GrafoListaArista.java
+++ Laboratorio 9/Ejercicio2/GrafoListaArista.java
@@ -1,29 +1,46 @@
 package Ejercicio2;
 
-import java.util.*;
-
 public class GrafoListaArista<T, E> {
-    private List<VerticeObjeto<T, E>> listaVertices;
-    private List<AristaObjeto<T, E>> listaAristas;
+    private VerticeObjeto<T, E>[] vertices;
+    private AristaObjeto<T, E>[] aristas;
+    private int cantidadVertices;
+    private int cantidadAristas;
 
     public GrafoListaArista() {
-        this.listaVertices = new ArrayList<>();
-        this.listaAristas = new ArrayList<>();
+        this.vertices = new VerticeObjeto[100];
+        this.aristas = new AristaObjeto[100];
+        this.cantidadVertices = 0;
+        this.cantidadAristas = 0;
     }
 
     public void insertarVertice(T dato) {
         if (!buscarVertice(dato)) {
-            VerticeObjeto<T, E> vertice = new VerticeObjeto<>(dato, listaVertices.size());
-            listaVertices.add(vertice);
+            vertices[cantidadVertices] = new VerticeObjeto<>(dato, cantidadVertices);
+            cantidadVertices++;
         }
     }
 
     public boolean buscarVertice(T dato) {
-        return listaVertices.stream().anyMatch(v -> v.obtenerInformacion().equals(dato));
+        for (int i = 0; i < cantidadVertices; i++) {
+            if (vertices[i].obtenerInformacion().equals(dato)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public VerticeObjeto<T, E> obtenerVertice(T dato) {
+        for (int i = 0; i < cantidadVertices; i++) {
+            if (vertices[i].obtenerInformacion().equals(dato)) {
+                return vertices[i];
+            }
+        }
+        return null;
     }
 
     public boolean buscarArista(T dato1, T dato2) {
-        for (AristaObjeto<T, E> arista : listaAristas) {
+        for (int i = 0; i < cantidadAristas; i++) {
+            AristaObjeto<T, E> arista = aristas[i];
             T v1 = arista.obtenerVertice1().obtenerInformacion();
             T v2 = arista.obtenerVertice2().obtenerInformacion();
             if ((v1.equals(dato1) && v2.equals(dato2)) || (v1.equals(dato2) && v2.equals(dato1))) {
@@ -35,46 +52,48 @@ public class GrafoListaArista<T, E> {
 
     public void insertarArista(T dato1, T dato2, E peso) {
         if (!buscarArista(dato1, dato2)) {
-            VerticeObjeto<T, E> vertice1 = obtenerVertice(dato1);
-            VerticeObjeto<T, E> vertice2 = obtenerVertice(dato2);
-            if (vertice1 != null && vertice2 != null) {
-                AristaObjeto<T, E> nueva = new AristaObjeto<>(vertice1, vertice2, peso, listaAristas.size());
-                listaAristas.add(nueva);
+            VerticeObjeto<T, E> v1 = obtenerVertice(dato1);
+            VerticeObjeto<T, E> v2 = obtenerVertice(dato2);
+            if (v1 != null && v2 != null) {
+                aristas[cantidadAristas] = new AristaObjeto<>(v1, v2, peso, cantidadAristas);
+                cantidadAristas++;
             }
         }
-    }
-
-    private VerticeObjeto<T, E> obtenerVertice(T dato) {
-        for (VerticeObjeto<T, E> vertice : listaVertices) {
-            if (vertice.obtenerInformacion().equals(dato)) {
-                return vertice;
-            }
-        }
-        return null;
     }
 
     public void recorridoAnchura(T inicio) {
-        Set<T> visitados = new HashSet<>();
-        Queue<T> cola = new LinkedList<>();
+        boolean[] visitado = new boolean[cantidadVertices];
+        T[] cola = (T[]) new Object[cantidadVertices];
+        int frente = 0, fin = 0;
 
-        visitados.add(inicio);
-        cola.offer(inicio);
+        VerticeObjeto<T, E> vInicio = obtenerVertice(inicio);
+        if (vInicio == null) return;
 
-        while (!cola.isEmpty()) {
-            T actual = cola.poll();
+        int posInicio = vInicio.obtenerPosicion();
+        visitado[posInicio] = true;
+        cola[fin++] = inicio;
+
+        while (frente < fin) {
+            T actual = cola[frente++];
             System.out.print(actual + " ");
 
-            for (AristaObjeto<T, E> arista : listaAristas) {
-                T vecino = null;
-                if (arista.obtenerVertice1().obtenerInformacion().equals(actual)) {
-                    vecino = arista.obtenerVertice2().obtenerInformacion();
-                } else if (arista.obtenerVertice2().obtenerInformacion().equals(actual)) {
-                    vecino = arista.obtenerVertice1().obtenerInformacion();
-                }
+            for (int i = 0; i < cantidadAristas; i++) {
+                AristaObjeto<T, E> arista = aristas[i];
+                T origen = arista.obtenerVertice1().obtenerInformacion();
+                T destino = arista.obtenerVertice2().obtenerInformacion();
 
-                if (vecino != null && !visitados.contains(vecino)) {
-                    visitados.add(vecino);
-                    cola.offer(vecino);
+                if (origen.equals(actual)) {
+                    int posDestino = arista.obtenerVertice2().obtenerPosicion();
+                    if (!visitado[posDestino]) {
+                        cola[fin++] = destino;
+                        visitado[posDestino] = true;
+                    }
+                } else if (destino.equals(actual)) {
+                    int posOrigen = arista.obtenerVertice1().obtenerPosicion();
+                    if (!visitado[posOrigen]) {
+                        cola[fin++] = origen;
+                        visitado[posOrigen] = true;
+                    }
                 }
             }
         }
@@ -83,13 +102,14 @@ public class GrafoListaArista<T, E> {
 
     public void imprimirGrafo() {
         System.out.println("Vértices:");
-        for (VerticeObjeto<T, E> v : listaVertices) {
-            System.out.println("- " + v.obtenerInformacion());
+        for (int i = 0; i < cantidadVertices; i++) {
+            System.out.println("- " + vertices[i].obtenerInformacion());
         }
 
         System.out.println("Aristas:");
-        for (AristaObjeto<T, E> a : listaAristas) {
-            System.out.println(a);
+        for (int i = 0; i < cantidadAristas; i++) {
+            System.out.println(aristas[i]);
         }
     }
 }
+
