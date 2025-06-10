5e98a608bf02d9d772e181237ef402211ab0f9fd
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-06-09 20:59:47 -0500

    Create VerticeObjeto.java

---
+++ Laboratorio 9/Ejercicio3/VerticeObjeto.java
@@ -0,0 +1,32 @@
+package Ejercicio3;
+
+public class VerticeObjeto<T, E> {
+    protected T informacion;
+    protected int posicion;
+
+    public VerticeObjeto(T informacion, int posicion) {
+        this.informacion = informacion;
+        this.posicion = posicion;
+    }
+
+    public T obtenerInformacion() {
+        return informacion;
+    }
+
+    public int obtenerPosicion() {
+        return posicion;
+    }
+
+    public void establecerInformacion(T informacion) {
+        this.informacion = informacion;
+    }
+
+    public void establecerPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    @Override
+    public String toString() {
+        return informacion.toString();
+    }
+}
