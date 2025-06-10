12d0eec766f7d2649197efdd6bdcf62f01118f08
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-06-10 13:38:25 -0500

    Update AristaObjeto.java

--- Laboratorio 9/Ejercicio2/AristaObjeto.java
+++ Laboratorio 9/Ejercicio2/AristaObjeto.java
@@ -1,36 +1,14 @@
 package Ejercicio2;
 
-public class AristaObjeto<T, E> {
-    private E informacion;
-    private VerticeObjeto<T, E> vertice1;
-    private VerticeObjeto<T, E> vertice2;
-    private int posicion;
+public class Arista {
+    public Vertice destino;
+    public int peso;
+    public Arista siguiente;
 
-    public AristaObjeto(VerticeObjeto<T, E> v1, VerticeObjeto<T, E> v2, E info, int posicion) {
-        this.vertice1 = v1;
-        this.vertice2 = v2;
-        this.informacion = info;
-        this.posicion = posicion;
-    }
-
-    public E obtenerInformacion() {
-        return informacion;
-    }
-
-    public VerticeObjeto<T, E> obtenerVertice1() {
-        return vertice1;
-    }
-
-    public VerticeObjeto<T, E> obtenerVertice2() {
-        return vertice2;
-    }
-
-    public int obtenerPosicion() {
-        return posicion;
-    }
-
-    public String toString() {
-        return "(" + vertice1 + " - " + vertice2 + ", peso: " + informacion + ")";
+    public Arista(Vertice destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+        this.siguiente = null;
     }
 }
 
