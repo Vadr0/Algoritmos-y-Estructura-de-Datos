9ff6d325e1265cf43c4aed19f8d87d1bc850e53e
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-06-21 17:32:27 -0500

    Se creo la clase BNode

---
+++ Laboratorio 10/btree/BNode.java
@@ -0,0 +1,19 @@
+package btree;
+
+import java.util.ArrayList;
+
+public class BNode<E> {
+    protected ArrayList<E> keys;
+    protected ArrayList<BNode<E>> childs;
+    protected int count;
+
+    public BNode (int n) {
+        this.keys = new ArrayList<E>(n);
+        this.childs = new ArrayList<BNode<E>>(n);
+        this.count = 0;
+        for (int i = 0; i < n;i++){
+            this.keys.add(null);
+            this.childs.add(null);
+        }
+    }
+}
