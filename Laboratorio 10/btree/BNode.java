59b3a096cd515a998580c1ed2c643861e4bc8949
M4FB <marcelo.avendano@ucsm.edu.pe>
2025-06-21 17:38:26 -0500

    Se añadio el metodo de busqueda y el tostring

--- Laboratorio 10/btree/BNode.java
+++ Laboratorio 10/btree/BNode.java
@@ -2,7 +2,7 @@ package btree;
 
 import java.util.ArrayList;
 
-public class BNode<E> {
+public class BNode<E extends Comparable<E>> {
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
@@ -27,8 +27,38 @@ public class BNode<E> {
         return count == 0;
     }
 
+    // Clase interna para el resultado de búsqueda
+    public static class SearchResult {
+        public boolean found;
+        public int position;
+        public SearchResult(boolean found, int position) {
+            this.found = found;
+            this.position = position;
+        }
+    }
+
+    // Busca una clave en el nodo actual
+    public SearchResult searchNode(E key) {
+        int i = 0;
+        while (i < count && keys.get(i) != null && key.compareTo(keys.get(i)) > 0) {
+            i++;
+        }
+
+        if (i < count && keys.get(i) != null && key.compareTo(keys.get(i)) == 0) {
+            return new SearchResult(true, i); // esta en la posición i
+        } else {
+            return new SearchResult(false, i); // se debe descender, no encontro
+        }
+    }
     
-
-
-
+    @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i < count; i++) {
+            result += keys.get(i);
+            if (i < count - 1) result += ", ";
+        }
+        result += "]";
+        return result;
+    }
 }
