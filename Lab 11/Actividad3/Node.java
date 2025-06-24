631955a6a7d036e2a566dd48b33d2c574454f647
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2025-06-23 21:29:53 -0500

    Create Node.java

---
+++ Lab 11/Actividad3/Node.java
@@ -0,0 +1,12 @@
+package Actividad3;
+
+// Nodo simple para una lista enlazada
+public class Node {
+    Register data;
+    Node next;
+
+    public Node(Register data) {
+        this.data = data;
+        this.next = null;
+    }
+}
