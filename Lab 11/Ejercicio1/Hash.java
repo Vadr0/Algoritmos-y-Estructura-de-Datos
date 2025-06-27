5e7fae89efb480c17c33d2a76d4076781730e52f
rodel2A <164429797+rodel2A@users.noreply.github.com>
2025-06-26 22:52:58 -0500

    Delete Lab 11/Ejercicio1 directory

--- Lab 11/Ejercicio1/Hash.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio1;
-
-public interface Hash {
-    void insertar(int clave, String valor); // Para tablas con valor asociado
-    void insertar(int clave); // Para tablas solo con claves
-    String buscar(int clave);
-    void eliminar(int clave);
-    void mostrar();
-}
